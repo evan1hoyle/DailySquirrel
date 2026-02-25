87a1c32b3e2bf5bde52e820c60d9ff3eb8ff2af2
evan1hoyle <102268467+evan1hoyle@users.noreply.github.com>
2026-02-25 09:42:52 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+UNSPLASH_ACCESS_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+SENDER_EMAIL = 'example@example.com'
+SENDER_PASSWORD = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+HISTORY_FILE = 'sent_squirrels.txt'
+LOG_FILE = 'squirrel_log.txt'
+RECIPIENTS = [
+    'example@example.com',
+    'example@example.com'
+]
